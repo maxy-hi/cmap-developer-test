387ca3e56cb39a1695b1c071595f60fe19968a38
maxyhi <maxyhi048+github@gmail.com>
2023-09-21 19:16:57 +0100

    tests for getting ordered items

--- Timesheets.Test/TimesheetTests.cs
+++ Timesheets.Test/TimesheetTests.cs
@@ -39,5 +39,129 @@ namespace Timesheets.Test
             // Assert
             mockRepository.Verify(repo => repo.AddTimesheet(It.IsAny<Timesheet>()), Times.Once);
         }
+
+
+        #region get ordered items
+        [Fact]
+        public void GivenAValidTimesheets_Get3OrderedItems()
+        {
+
+            var mockRepository = new Mock<ITimesheetRepository>();
+            mockRepository
+                .Setup(x => x.GetProjectInfos())
+                .Returns(new List<ProjectInfo> {
+                    new ProjectInfo
+                    {
+                        Project = "1",
+                        TotalHours = 6.0,
+                        Workers =
+                        new List<Workers>
+                        {
+                            new Workers
+                            {
+                                Name = "TEST 1",
+                                HoursWorked = 6,
+                                Date = "12/12/12",
+                            }
+                        }
+                    },
+                    new ProjectInfo
+                    {
+                        Project = "2",
+                        TotalHours = 1.0,
+                        Workers =
+                        new List<Workers>
+                        {
+                            new Workers
+                            {
+                                Name = "TEST 2",
+                                HoursWorked = 1,
+                                Date = "11/12/12",
+                            }
+                        }
+                    },
+                    new ProjectInfo
+                    {
+                        Project = "3",
+                        TotalHours = 2.0,
+                        Workers =
+                        new List<Workers>
+                        {
+                            new Workers
+                            {
+                                Name = "TEST 3",
+                                HoursWorked = 2,
+                                Date = "11/12/12",
+                            }
+                        }
+                    },
+                });
+
+
+            var timesheetService = new TimesheetService(mockRepository.Object);
+
+            var sorted = timesheetService.GetProjectInfos();
+
+            Assert.NotNull(sorted);
+            Assert.NotEmpty(sorted);
+            Assert.Equal(6.0, sorted.First().TotalHours);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheets_Get1OrderedItems()
+        {
+
+            var mockRepository = new Mock<ITimesheetRepository>();
+            mockRepository
+                .Setup(x => x.GetProjectInfos())
+                .Returns(new List<ProjectInfo> {
+                    new ProjectInfo
+                    {
+                        Project = "1",
+                        TotalHours = 6.0,
+                        Workers =
+                        new List<Workers>
+                        {
+                            new Workers
+                            {
+                                Name = "TEST 1",
+                                HoursWorked = 6,
+                                Date = "12/12/12",
+                            }
+                        }
+                    },
+                });
+
+
+            var timesheetService = new TimesheetService(mockRepository.Object);
+
+            var sorted = timesheetService.GetProjectInfos();
+
+            Assert.NotNull(sorted);
+            Assert.NotEmpty(sorted);
+            Assert.Single(sorted);
+            Assert.Equal(6.0, sorted.First().TotalHours);
+        }
+
+        [Fact]
+        public void GivenAValidTimesheets_Get0OrderedItems()
+        {
+
+            var mockRepository = new Mock<ITimesheetRepository>();
+            mockRepository
+                .Setup(x => x.GetProjectInfos())
+                .Returns(new List<ProjectInfo>
+                {
+                });
+
+
+            var timesheetService = new TimesheetService(mockRepository.Object);
+
+            var sorted = timesheetService.GetProjectInfos();
+
+            Assert.NotNull(sorted);
+            Assert.Empty(sorted);
+        }
+        #endregion
     }
 }
